a7fcd8115cb37a1a45831bf2b1f4e103e2531216
carlitroxxx <ca.moil@duocuc.cl>
2026-09-08 14:51:29 -0300

    Version 1.5.3: FIX SecurityConfig para despliegue

--- src/main/java/com/snkrlab/productos/config/SecurityConfig.java
+++ src/main/java/com/snkrlab/productos/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("*", "http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
